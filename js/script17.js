f60320d1948e285f5807901346da4161eb5b61c9
Солтис Владислав <vladyslav.soltys@ugv.com.ua>
2023-10-20 11:18:02 +0300

    20/10/2023

---
+++ js/script17.js
@@ -0,0 +1,30 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+setTimeout(() => {                           // складова WEB API браузера
+    console.log("hello from SetTimeout");
+}, 0);
+
+
+console.log(6);
+console.log(7);
+for (let i = 0; i < 10; i++) {
+   console.log("Soltech");  
+};
+console.log(8);
+function sayHello() {
+    console.log("hello");
+};
+
+sayHello();
+
+console.log(9);
+console.log(10);
+
+// Non-blocking
+// AJAX
+
+// парадигми : синхронний і асинхронний код
---
+++ js/script18.js
@@ -0,0 +1,26 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+// fetch повертає Promise-вбудований об'єкт
+
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+      console.log(6);
+      console.log(7);
+
+      
+fetch('https://jsonplaceholder.typicode.com/photos')
+.then(response => response.json())
+.then(json => console.log(json))
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
